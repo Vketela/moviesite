ab5beae3d0370caab8f419aee14f62458c18bb13
Vktela <ketelaarvanessa@gmail.com>
2024-02-20 20:23:39 +0100

    Moviecard component

---
+++ src/components/MovieCardDeck/MovieCardDeck.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './MovieCardDeck.css';
+
+const MovieCardDeck = () => (
+  <div className="MovieCardDeck">
+    MovieCardDeck Component
+  </div>
+);
+
+MovieCardDeck.propTypes = {};
+
+MovieCardDeck.defaultProps = {};
+
+export default MovieCardDeck;
