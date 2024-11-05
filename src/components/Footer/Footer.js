55a166bd69aaff097f2f99e79cb01ba48478d63b
Vktela <ketelaarvanessa@gmail.com>
2024-11-05 22:54:48 +0100

    componenten bijgewerkt en uiterlijk verbeterd.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css';
 const Footer = () => (
   <div className="Footer">
     <footer>
-      © 2024 POPCORN Limited. Handelsmerk POPCORN en al het intellectuele eigendom dat zij bevat, <br />
+      © 2024 POPCORN Limited. Handelsmerk POPCORN en al het intellectuele eigendom dat zij bevat, 
       zijn eigendom van POPCORN Networks Inc.,  Alle andere handelsmerken en het intellectuele<br />
       eigendom zijn het bezit van hun respectievelijke eigenaren.  Alle rechten voorbehouden.
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,12 +3,18 @@ import PropTypes from 'prop-types';
 import './Header.css';
 import Banner from '../Banner/Banner';
 import popcornbeker from '../../images/popcornbeker.png';
+import filmhero from '../../images/filmhero.jpg';
 
 
 const Header = () => {
   return (
-    <div className="header">
-    <h1>POPCORN</h1>
+    <div style={{ backgroundImage: `url(${filmhero})` }} className="header">
+
+
+      <h1 className='filmhero'>POPCORN</h1>
+      <h2>Ontdek en kijk naar je favoriete films!</h2>
+
+
       <img src={popcornbeker} alt="Popcorn Beker" />
       <div className="column-wrapper">
         <form action="/search" method="get" className="search-form">
@@ -22,10 +28,9 @@ const Header = () => {
           <button type="submit" className="search-button">search</button>
         </form>
       </div>
-      <Banner /> {/* Zorg ervoor dat de Banner ook wordt weergegeven */}
     </div>
-    
-  
+
+
   );
 };
 
